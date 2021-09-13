999fbf9c57deec142ec027c29440f67285b92ce2
Marc Bender <marc_bender@icloud.com>
2021-09-13 19:12:44 +0200

    Updated to Maccatalyst support

---
+++ iphone/build/Intermediates.noindex/ArchiveIntermediates/tibezier/IntermediateBuildFilesPath/tibezier.build/Release-maccatalyst/tibezier.build/Objects-normal/arm64/GuyMcdoooooTibezierModule.d
@@ -0,0 +1,31 @@
+dependencies: \
+  /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.0.sdk/SDKSettings.json \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/GuyMcdoooooTibezier_Prefix.pch \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/Classes/GuyMcdoooooTibezierModule.m \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/Classes/GuyMcdoooooTibezierModule.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/include/TiModule.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiModule.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/Module.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiEvaluator.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/KrollContext.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiProxy.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/KrollCallback.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/KrollWrapper.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/KrollObject.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiBindingRunLoop.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiBase.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiSharedConfig.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiThreading.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiPublicAPI.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiUtils.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiDimension.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiBuffer.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiColor.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiFile.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/WebFont.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/include/TiBase.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/include/TiHost.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiHost.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/Bridge.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/Frameworks/TitaniumKit.xcframework/ios-arm64/TitaniumKit.framework/Headers/TiStylesheet.h \
+  /Users/marcbender/Library/Application\ Support/Titanium/mobilesdk/osx/10.0.2.GAcustom/iphone/include/TiUtils.h
---
+++ iphone/build/Intermediates.noindex/ArchiveIntermediates/tibezier/IntermediateBuildFilesPath/tibezier.build/Release-maccatalyst/tibezier.build/Objects-normal/arm64/GuyMcdoooooTibezierModuleAssets.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.0.sdk/SDKSettings.json \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/GuyMcdoooooTibezier_Prefix.pch \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/Classes/GuyMcdoooooTibezierModuleAssets.m \
+  /Users/marcbender/Downloads/Tibezier-master/iphone/Classes/GuyMcdoooooTibezierModuleAssets.h
